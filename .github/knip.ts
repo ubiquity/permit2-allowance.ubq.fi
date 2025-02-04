b8281614628146481470f080149b53319dc5950f
zugdev <zug.dev@proton.me>
2025-02-04 17:58:56 -0300

    feat: fix knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,12 +1,13 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["build/index.ts", ".github/empty-string-checker.ts"],
+  entry: ["static/main.ts", ".github/empty-string-checker.ts"],
   project: ["src/**/*.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "eslint.config.mjs"],
   ignoreExportsUsedInFile: true,
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
+  ignoreDependencies: ["dotenv", "@mswjs/data"],
+  ignoreBinaries: ["cypress/scripts/anvil.sh"],
   eslint: true,
 };
 
