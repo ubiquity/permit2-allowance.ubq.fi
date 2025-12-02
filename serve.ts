9a6c2bdc1c76488a77c501ce6bc77e3b5e57d37e
0x4007 <gpg@pavlovcik.com>
2025-12-02 02:53:13 -0500

    chore: serve full static root

--- serve.ts
+++ serve.ts
@@ -1,5 +1,5 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
-const root = Deno.env.get("STATIC_DIR") ?? "static/dist";
+const root = Deno.env.get("STATIC_DIR") ?? "static";
 
 Deno.serve((req) => serveDir(req, { fsRoot: root, quiet: true }));
