ea261c0e905efd0d6d75f339af92fbb38eb69553
0x4007 <gpg@pavlovcik.com>
2025-12-02 02:14:19 -0500

    chore: address copilot review

--- serve.ts
+++ serve.ts
@@ -1,4 +1,4 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
+import { serveDir } from "jsr:@std/http/file-server";
 
 const root = Deno.env.get("STATIC_DIR") ?? "static/dist";
 
